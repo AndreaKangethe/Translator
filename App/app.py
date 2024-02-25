d2cfbb4c69fd91836c1f6d1ed3a11e0013f9ea7e
Andrea Kangethe <152164886+AndreaKangethe@users.noreply.github.com>
2024-02-25 16:18:11 +0300

    Add files via upload

---
+++ App/app.py
@@ -0,0 +1,8 @@
+from flask import Flask, redirect, url_for, request, render_template, session
+import requests, os, uuid, json
+from dotenv import load_dotenv
+load_dotenv()
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
